09489f167f531ffd45a3ab9d30f905c1c5b41e07
Szymon Rozek <rozek1szymon@gmail.com>
2024-09-20 19:49:36 +0200

    add service plan module

---
+++ modules/azure/service_plan/main.tf
@@ -0,0 +1,117 @@
+terraform {
+  required_version = "~> 1.1"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.48"
+    }
+  }
+
+  backend "azurerm" {}
+}
+
+provider "azurerm" {
+  features {}
+}
+
+locals {
+  scale_in_threshold_rules = [
+    for rule in var.scaling_rules : {
+      threshold   = rule.scale_in_threshold
+      metric_name = rule.metric_name
+      direction   = "Decrease"
+      operator    = "LessThan"
+    }
+  ]
+
+  scale_out_threshold_rules = [
+    for rule in var.scaling_rules : {
+      threshold   = rule.scale_out_threshold
+      metric_name = rule.metric_name
+      direction   = "Increase"
+      operator    = "GreaterThan"
+    }
+  ]
+}
+
+resource "azurerm_service_plan" "sp" {
+  name                = var.name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  os_type             = var.os_type
+  sku_name            = var.sku_name
+}
+
+resource "azurerm_monitor_autoscale_setting" "autoscale_setting" {
+  count               = var.enable_autoscaling ? 1 : 0
+  name                = "scale-${var.name}"
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  target_resource_id  = azurerm_service_plan.sp.id
+
+  profile {
+    name = "scale-${var.name}"
+
+    capacity {
+      default = var.default_scaling_capacity
+      minimum = var.minimum_scaling_capacity
+      maximum = var.maximum_scaling_capacity
+    }
+
+    dynamic "rule" {
+      for_each = concat(local.scale_in_threshold_rules, local.scale_out_threshold_rules)
+
+      content {
+        metric_trigger {
+          metric_name        = rule.value.metric_name
+          metric_resource_id = azurerm_service_plan.sp.id
+          time_grain         = "PT1M"
+          statistic          = "Average"
+          time_window        = "PT5M"
+          time_aggregation   = "Average"
+          operator           = rule.value.operator
+          threshold          = rule.value.threshold
+        }
+
+        scale_action {
+          direction = rule.value.direction
+          type      = "ChangeCount"
+          value     = "1"
+          cooldown  = "PT1M"
+        }
+      }
+    }
+  }
+}
+
+
+data "azurerm_monitor_diagnostic_categories" "diagnostic_categories" {
+  count       = var.loganalytics_diagnostic_setting == null ? 0 : 1
+  resource_id = azurerm_service_plan.sp.id
+}
+
+resource "azurerm_monitor_diagnostic_setting" "diagnostic_setting" {
+  count                      = var.loganalytics_diagnostic_setting == null ? 0 : 1
+  name                       = "diag-${var.name}"
+  target_resource_id         = azurerm_service_plan.sp.id
+  log_analytics_workspace_id = var.loganalytics_diagnostic_setting.workspace_id
+
+
+  dynamic "enabled_log" {
+    for_each = var.loganalytics_diagnostic_setting.categories == null ? data.azurerm_monitor_diagnostic_categories.diagnostic_categories[0].log_category_types : var.loganalytics_diagnostic_setting.categories
+
+    content {
+      category = enabled_log.value
+    }
+  }
+
+  dynamic "metric" {
+    for_each = var.loganalytics_diagnostic_setting.metrics == null ? data.azurerm_monitor_diagnostic_categories.diagnostic_categories[0].metrics : var.loganalytics_diagnostic_setting.metrics
+
+    content {
+      category = metric.value
+      enabled  = true
+    }
+  }
+}
---
+++ modules/azure/service_plan/variables.tf
@@ -0,0 +1,78 @@
+variable "location" {
+  type        = string
+  description = "A datacenter location in Azure."
+}
+
+variable "resource_group_name" {
+  type        = string
+  description = "Name of the resource group."
+}
+
+variable "name" {
+  type        = string
+  description = "Name of the service plan."
+}
+
+# Possible values: https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/service_plan#sku_name
+variable "sku_name" {
+  type        = string
+  description = "The SKU for the plan. Possible values can be found on https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/service_plan#sku_name"
+}
+
+# Possible values: https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/service_plan#os_type
+variable "os_type" {
+  type        = string
+  description = "The OS type for the service plan. Possible values can be found on https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/service_plan#os_type"
+}
+
+variable "enable_autoscaling" {
+  type        = bool
+  description = "Indicates whether autoscaling should be turned on."
+  default     = false
+}
+
+variable "default_scaling_capacity" {
+  type        = number
+  description = "The number of instances that are available for scaling if metrics are not available for evaluation."
+  default     = 1
+}
+
+variable "minimum_scaling_capacity" {
+  type        = number
+  description = "The minimum number of instances for this resource."
+  default     = 1
+}
+
+variable "maximum_scaling_capacity" {
+  type        = number
+  description = "The maximum number of instances for this resource."
+  default     = 3
+}
+
+variable "scaling_rules" {
+  type = set(object({
+    metric_name         = string
+    scale_out_threshold = number
+    scale_in_threshold  = number
+  }))
+  description = "Scaling rules for autoscaling."
+
+  default = [
+    {
+      metric_name         = "CpuPercentage"
+      scale_out_threshold = 80
+      scale_in_threshold  = 20
+    }
+  ]
+}
+
+
+variable "loganalytics_diagnostic_setting" {
+  type = object({
+    workspace_id = string,                 // log analytics workspace ID
+    categories   = optional(list(string)), // null list,means send all categories
+    metrics      = optional(list(string)), // null list means send all metrics
+  })
+  description = "Specifies the log categories that have to be sent to Log analytics."
+  default     = null
+}
