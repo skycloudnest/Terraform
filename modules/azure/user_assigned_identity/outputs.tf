616ee2643ddea9f898990646fdb320512daa91fe
Szymon Rozek <rozek1szymon@gmail.com>
2024-09-22 16:19:19 +0200

    add client id

--- modules/azure/user_assigned_identity/outputs.tf
+++ modules/azure/user_assigned_identity/outputs.tf
@@ -2,6 +2,10 @@ output "principal_id" {
   value = azurerm_user_assigned_identity.identity.principal_id
 }
 
+output "client_id" {
+  value = azurerm_user_assigned_identity.identity.client_id
+}
+
 output "id" {
   value = azurerm_user_assigned_identity.identity.id
 }
