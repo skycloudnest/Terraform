ce1db95418b5b279a8bfadd7adc4df06426fd198
Szymon Rozek <rozek1szymon@gmail.com>
2024-09-25 19:52:45 +0200

    Add apim api module

---
+++ modules/azure/api_management_api/outputs.tf
@@ -0,0 +1,23 @@
+output "api_name" {
+  value = azurerm_api_management_api.api.name
+}
+
+output "api_management_api_diagnostic" {
+  value = (var.api_diagnostic_settings != null && var.api_management_logger_id != null) ? azurerm_api_management_api_diagnostic.api_diagnostic[0].id : null
+}
+
+output "application_id" {
+  value = azuread_application.application.application_id
+}
+
+output "default_role" {
+  value = azuread_application.application.app_role_ids["Default.Access"]
+}
+
+output "api_audiences" {
+  value = azuread_application.application.identifier_uris
+}
+
+output "api_audience" {
+  value = "api://${local.app_api_endpoint}"
+}
